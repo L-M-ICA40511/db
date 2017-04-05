69817622b1deacda1e7bf53a8fd958b689407f74
LFBarreto <lfb.access@gmail.com>
2017-04-05 14:06:53 +0200

    add clear and fix some typings for aot compilation

--- src/database.ts
+++ src/database.ts
@@ -6,7 +6,7 @@ import { mergeMap } from 'rxjs/operator/mergeMap';
 import { map } from 'rxjs/operator/map';
 import { _do } from 'rxjs/operator/do';
 import { from } from 'rxjs/observable/from';
-import { OpaqueToken, Inject, Injectable, NgModule, ModuleWithProviders } from '@angular/core';
+import { OpaqueToken, Inject, NgModule, ModuleWithProviders } from '@angular/core';
 
 
 const IDB_SUCCESS = 'success';
@@ -37,19 +37,18 @@ export interface DBSchema {
   stores: {[storename: string]: DBStore};
 }
 
-export function getIDBFactory(): IDBFactory {
+export function getIDBFactory() {
   return typeof window !== 'undefined' ? window.indexedDB : self.indexedDB;
 }
 
-@Injectable()
 export class Database {
 
   public changes: Subject<any> = new Subject();
 
-  private _idb: IDBFactory;
+  private _idb;
   private _schema: DBSchema;
 
-  constructor(@Inject(DatabaseBackend) idbBackend: any, @Inject(IDB_SCHEMA) schema: any) {
+  constructor(@Inject(DatabaseBackend) idbBackend, @Inject(IDB_SCHEMA) schema: DBSchema) {
     this._schema = schema;
     this._idb = idbBackend;
   }
@@ -269,6 +268,38 @@ export class Database {
 
   compare(a: any, b: any): number {
     return this._idb.cmp(a, b);
+  }
+
+  clear(storeName: string) {
+    const open$ = this.open(this._schema.name);
+    return mergeMap.call(open$, (db: IDBDatabase) => {
+        return new Observable( (txnObserver: Observer<any>) => {
+         const recordSchema = this._schema.stores[storeName];
+         const mapper = this._mapRecord(recordSchema);
+         const txn = db.transaction([storeName], IDB_TXN_READWRITE);
+         const objectStore = txn.objectStore(storeName);
+
+         const clearRequest = objectStore.clear();
+
+         const onTxnError = (err: any) => txnObserver.error(err);
+         const onTxnComplete = () => txnObserver.complete();
+         const onClear = () => txnObserver.next(null);
+
+         txn.addEventListener(IDB_COMPLETE, onTxnComplete);
+         txn.addEventListener(IDB_ERROR, onTxnError);
+
+         clearRequest.addEventListener(IDB_SUCCESS, onClear);
+         clearRequest.addEventListener(IDB_ERROR, onTxnError);
+
+         return () => {
+           clearRequest.removeEventListener(IDB_SUCCESS, onClear);
+           clearRequest.removeEventListener(IDB_ERROR, onTxnError);
+           txn.removeEventListener(IDB_COMPLETE, onTxnComplete);
+           txn.removeEventListener(IDB_ERROR, onTxnError);
+         };
+
+        });
+      });
   }
 }
 
